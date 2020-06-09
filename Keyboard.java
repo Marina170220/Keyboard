b497bf77bd10bc94bba4d61c8c6e76c888d50506
Marina <petrukmn@yandex.ru>
2020-06-09 08:03:50 +0300

    Add a keyboard type

--- Keyboard.java
+++ Keyboard.java
@@ -1,16 +1,20 @@
+import java.util.Scanner;
+
 public class Keyboard implements Comparable<Keyboard> {
     String name;
     Double price;
     String color;
     boolean light;
     int amount;
+    String keyType;
 
-    Keyboard(String name, Double price, String color, boolean light, int amount) {
+    Keyboard(String name, Double price, String color, boolean light, int amount, String keyType) {
         this.name = name;
         this.price = price;
         this.color = color;
         this.light = light;
         this.amount = amount;
+        this.keyType = keyType;
     }
 
     public String getName() {
@@ -23,6 +27,18 @@ public class Keyboard implements Comparable<Keyboard> {
 
     public int getAmount() {
         return amount;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public boolean isLight() {
+        return light;
+    }
+
+    public String getKeyType() {
+        return keyType;
     }
 
     @Override
