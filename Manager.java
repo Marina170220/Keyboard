46ad97bd3f19f0afcfe76865fb306a3049b55d31
Marina <petrukmn@yandex.ru>
2020-06-09 08:06:08 +0300

    Change compare methods, add a find color, choose backlight & find keyboard type methods

--- Manager.java
+++ Manager.java
@@ -1,5 +1,8 @@
 public class Manager {
     String nameEnteredByUser;
+    String colorEnteredByUser;
+    String keyTypeEnteredByUser;
+    String lightEnteredByUser;
     int counter = 0;
 
     Double findOneItemCost(Keyboard k) {
@@ -7,7 +10,35 @@ public class Manager {
     }
 
     void findName(Keyboard k) {
-        if (k.getName().toLowerCase().equals(nameEnteredByUser)) {
+        boolean compareName = nameEnteredByUser.regionMatches(true, 0, k.getName(), 0, 3);
+        if (compareName) {
+            System.out.println(k.toString());
+            counter++;
+        }
+    }
+
+    void showColor(Keyboard k) {
+        boolean compareColor = colorEnteredByUser.regionMatches(true, 0, k.getColor(), 0, 4);
+        if (compareColor) {
+            System.out.println(k.toString());
+            counter++;
+        }
+    }
+
+    void showLight(Keyboard k) {
+        boolean light = lightEnteredByUser.trim().toLowerCase().startsWith("y");
+        if (light && k.isLight()) {
+            System.out.println(k.toString());
+            counter++;
+        } else if (!light && !k.isLight()) {
+            System.out.println(k.toString());
+            counter++;
+        }
+    }
+
+    void showKeyType(Keyboard k) {
+        boolean compareKeyType = keyTypeEnteredByUser.regionMatches(true, 0, k.getKeyType(), 0, 4);
+        if (compareKeyType) {
             System.out.println(k.toString());
             counter++;
         }
